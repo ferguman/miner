ce65ff76cfb6bbe850974ba34d9afe6833b9d086
ferguman <ferguman@gmail.com>
2020-04-11 09:34:20 -0500

    fopd module

---
+++ fopdm.py
@@ -0,0 +1,21 @@
+# fopdm -> Farm Operations Device Monero Miner
+# This is a fopd module that one can use to mine monero
+# It is not enabled by default and thus the fopd operator has control of 
+# whether or not to use it.
+# It can be configured to mine to the benefit of the fopd Monero account or
+# the user can configure to mine to the benefit of their choice of Monero
+# account.  The user can also choose to run mulitple copies running to 
+# the benefit of mulitple Monero accounts. The percentage sharing among
+# mulitple copies is also completely configurable by the user.
+#
+# The miner is designed not to be intrusive and thus it does not
+# intentionally interfere with or overload it's host system.  
+#
+# Turn it on or off as you will by editing the fopd config.py file.
+#
+# We hope you choose to run it and apply some of it's benefit to the
+# fopd teams Monero key. This will allow the fopd team to derive some
+# revenue from its efforts.
+#
+# To enalbe to run for the beneif of the fopd team simply run [TODO]
+#
